de2f84eb380bba2ce59ab4fbec3a17377b64458c
Jayasuriya-R <suryaabi3801@gmail.com>
2025-05-15 18:46:42 +0530

    fixed url issue

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -2,15 +2,17 @@ import React from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faTrash} from '@fortawesome/free-solid-svg-icons'
 import {faPenToSquare} from'@fortawesome/free-solid-svg-icons'
-
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
 const Todo = ({task,toggleComplete,deleteTodo,editTodo}) => {
   return (
     <div className='Todo'>
+      <FontAwesomeIcon
+           icon={faCheck}
            onClick={() => toggleComplete(task.id)}
            className={`${task.completed ? "completed" : "incompleted"}`}
-      
-      <p>{task.task}</p>
+      />
+         <p>{task.task}</p>
     
       <div>
         <FontAwesomeIcon icon ={faPenToSquare} onClick={()=>
